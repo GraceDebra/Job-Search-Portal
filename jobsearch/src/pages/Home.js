1338480e667e1cc47c8b4313f263a63616229bba
Gimo <kgimase@gmail.com>
2023-01-06 11:11:18 +0300

    companycard

---
+++ jobsearch/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box, SimpleGrid ,Heading,Text} from '@chakra-ui/react'
+import HomeCard from '../cards/HomeCard'
+import { data } from '../constants/Data'
+const Home = () => {
+  return (
+    <Box>
+    <Heading>
+        Recommended Companies
+    </Heading>
+    <Text>Based on your profile, company preferences and recent activity</Text>
+    <Box px="5vw">
+    <SimpleGrid mt="6vh" columns={[1, 2, 3, 4]} spacing="auto"  >
+      {data.map(content => {
+        return <HomeCard key={content.id} content={content} />;
+      })}
+    </SimpleGrid>
+  </Box>
+  </Box>
+  )
+}
+
+export default Home
